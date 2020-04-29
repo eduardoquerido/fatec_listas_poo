87ced3b0c86a32572fc8560398087f4e7a4f9257
Eduardo Querido <eduardo.rumbels@gmail.com>
2020-04-28 22:43:05 -0300

    Alterações das classes

--- br.fatec.lista1/src/br/com/negocio/fatec/Menu.java
+++ br.fatec.lista1/src/br/com/negocio/fatec/Menu.java
@@ -1,27 +1,27 @@
 package br.com.negocio.fatec;
 
 public class Menu {
-	public void boasVindas() {
-		System.out.println("Bem vindo a melhor agenda do mundo!");
+	public void menuAgenda() {
+		System.out.println("Bem Vindo a sua agenda de Contatos");
 	}
 	public void menu() {
-		System.out.println("### Menu ###");
+		System.out.println("+++++++++ Menu +++++++++");
 		System.out.println("1 - Inserir contato");
 		System.out.println("2 - Listar contatos");
 		System.out.println("3 - Editar contato");
 		System.out.println("4 - Excluir contato");
-		System.out.println("5 - Listar contatos ordem alfabetica");
-		System.out.println("6 - Listar por genero");
-		System.out.println("7 - Adicionar produto");
-		System.out.println("8 - Listar Clientes com Produtos");
-		System.out.println("9 - Relatorios");
+		System.out.println("5 - Listar contatos ordem alfabética");
+		System.out.println("6 - Listar por Gênero");
+		System.out.println("7 - Adicionar novo Produto");
+		System.out.println("8 - Listar clientes com seus Produtos");
+		System.out.println("9 - Relatórios");
 		System.out.println("0 - Sair");
 	}
 	
 	public void menu2() {
-		System.out.println("1 - Relatorio Idade media");
-		System.out.println("2 - Relatorio Idade media por genero");
-		System.out.println("3 - Relatorio produto mais consumido");
-		System.out.println("4 - Relatorio produto mais consumido por genero");
+		System.out.println("1 - Média de idade dos clientes");
+		System.out.println("2 - Média de idade dos cliente por gênero");
+		System.out.println("3 - Qual produto mais consumido");
+		System.out.println("4 - Qual produto mais consumido por gênero");
 	}
 }
